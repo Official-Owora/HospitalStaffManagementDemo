0b732036a48886f9ed2f897b7741ab757f04beba
Official-Owora <chukwuemeka.owora@gmail.com>
2023-05-08 14:50:32 +0100

    Add project files.

---
+++ src/backend/HSMD.Application/Contracts/IStaffRepository.cs
@@ -0,0 +1,18 @@
+﻿using HSMD.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSMD.Application.Contracts
+{
+    public interface IStaffRepository
+    {
+        Task<IEnumerable<Staff>> GetAllStaffAsync(bool trackChanges);
+        Task<Staff> GetStaffByIdAsync(int Id, bool trackChanges);
+        Task<Hospital> GetStaffByName(string FirstName, string LastName, bool trackChanges);
+        void CreatStaff(Staff staff);
+        void DeleteStaff(Staff staff);
+    }
+}
